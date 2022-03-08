fcafb928513f83b3acd39a270b99b39c59f0a2fc
artem <ayrapetovai@yandex.ru>
2022-03-08 20:09:12 +0300

    session options refinement

--- src/main/java/com/example/smpp/client/SmppClientOptions.java
+++ src/main/java/com/example/smpp/client/SmppClientOptions.java
@@ -5,19 +5,20 @@ import io.vertx.core.net.NetClientOptions;
 // R - хотелось бы менять в рантайме
 // S - это поле должно иметь одно и тоже значение на протяжении всего времени работы сессии
 public class SmppClientOptions extends NetClientOptions {
-// S  private String name; // per session
+// blueprint
+// S  private String host;  // per client
+// S  private int port;  // per client
+// S  private long connectTimeout;  // per client
+// S  private byte interfaceVersion;  // per client
 
-// configuration settings
 
+// draft
 // S  private SmppBindType type;  // per session
 // S  private String systemId;  // per session
 // S  private String password;  // per session
 // S  private String systemType;  // per session
-// S  private byte interfaceVersion;  // all sessions, interface version requested by us or them
 // S  private Address addressRange;  // per session: ton, npi, address
 // S  private long bindTimeout;       //  per session, length of time to wait for a bind response
-
-//    // if > 0, then activated
 // R  private long requestExpiryTimeout; // per session
 // R  private int windowSize; // per session
 // R  private long windowWaitTimeout; // per session
--- src/main/java/com/example/smpp/server/SmppServerOptions.java
+++ src/main/java/com/example/smpp/server/SmppServerOptions.java
@@ -5,25 +5,27 @@ import io.vertx.core.net.NetServerOptions;
 // R - хотелось бы менять в рантайме
 // S - это поле должно иметь одно и тоже значение на протяжении всего времени работы сессии
 public class SmppServerOptions extends NetServerOptions {
+// blueprint
+// S  private String name; // per server, for identification on local JVM?
+// R  private int maxConnectionSize; // per server
+// S  private boolean autoNegotiateInterfaceVersion; // per server
+// S  private byte interfaceVersion;// per server, smpp version the server supports
+// S  private boolean reuseAddress; // per server
+// S  private boolean jmxEnabled; // per server
+// S  private String jmxDomain; // per server
+
+
+// draft
 // R  boolean dropAllOnUnbind = false; // per session? При получении unbind, не досылать resp и другие pdu, сразу убить сессию
 // R  boolean replyToUnbind = true; //per session? В ответ на unbind посылать unbind_resp
 // R  Duration bindTimeout; // per session? Время, за которое сервер выдаст bind_resp
 // R  Duration unbindTimeout; // per session? Время, которе дается клиенту на отправку unbind_resp, по истечении все pdu в окне дропаются и соединение закрывается
 
 // R  private int windowSize; // per session?
-// S  private String name; // for identification on local JVM?
 // S  private String systemId; // per session?
 
 // R  private long defaultWindowWaitTimeout; // per session? (aquire(..., TimeUnit.))
 // R  private long defaultRequestExpiryTimeout; // per session?
 // R  private long defaultWindowMonitorInterval; // per session?
 // R  private boolean defaultSessionCountersEnabled = false; // per session?
-
-// R  private int maxConnectionSize;
-// S  private boolean autoNegotiateInterfaceVersion;
-// S  private byte interfaceVersion;// smpp version the server supports
-// S  private boolean reuseAddress;
-
-// S  private boolean jmxEnabled;
-// S  private String jmxDomain;
 }
